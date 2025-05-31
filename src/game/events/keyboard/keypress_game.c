da4b010ef9cb71cc7d6b39f492bcf97bcbe015de
Nicolas Metais <nico.metais@hotmail.fr>
2025-06-01 00:13:55 +0200

    switch weapons

--- src/game/events/keyboard/keypress_game.c
+++ src/game/events/keyboard/keypress_game.c
@@ -6,7 +6,7 @@
 /*   By: nmetais <nmetais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 16:38:24 by nmetais           #+#    #+#             */
-/*   Updated: 2025/05/30 18:00:32 by nmetais          ###   ########.fr       */
+/*   Updated: 2025/06/01 00:13:08 by nmetais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,38 @@
 
 void	on_keypress_game(int key, t_core *core)
 {
-	if (key == 65361 || key == 'a')
+	if (key == XK_Left || key == XK_a || key == XK_A)
 		core->player->key_left = true;
-	else if (key == 65363 || key == 'd')
+	else if (key == XK_Right || key == XK_d || key == XK_D)
 		core->player->key_right = true;
-	else if (key == 65362 || key == 'w')
+	else if (key == XK_Up || key == XK_w || key == XK_W)
 		core->player->key_up = true;
-	else if (key == 65364 || key == 's')
+	else if (key == XK_Down || key == XK_s || key == XK_S)
 		core->player->key_down = true;
+	if (key == XK_1 && core->player->weapon[1].owned)
+		core->player->current_weapon = 1;
+	if (key == XK_2 && core->player->weapon[2].owned)
+		core->player->current_weapon = 2;
+	if (key == XK_3 && core->player->weapon[3].owned)
+		core->player->current_weapon = 3;
+	if (key == XK_5 && core->player->weapon[5].owned)
+		core->player->current_weapon = 5;
+	if (key == XK_6 && core->player->weapon[6].owned)
+		core->player->current_weapon = 6;
+	if (key == XK_7 && core->player->weapon[7].owned)
+		core->player->current_weapon = 7;
+
 }
 
 void	on_keyrelease_game(int key, t_core *core)
 {
-	if (key == 65361 || key == 'a')
+	if (key == XK_Left || key == XK_a || key == XK_A)
 		core->player->key_left = false;
-	else if (key == 65363 || key == 'd')
+	else if (key == XK_Right || key == XK_d || key == XK_D)
 		core->player->key_right = false;
-	else if (key == 65362 || key == 'w')
+	else if (key == XK_Up || key == XK_w || key == XK_W)
 		core->player->key_up = false;
-	else if (key == 65364 || key == 's')
+	else if (key == XK_Down || key == XK_s || key == XK_S)
 		core->player->key_down = false;
 }
 
